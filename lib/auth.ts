1991f4cabf4e329c3002edba336d64769d96fced
Ajay <mailtoajay7007@gmail.com>
2025-07-29 07:39:23 +0530

    credentials logic

---
+++ lib/auth.ts
@@ -0,0 +1,49 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+
+
+export const authOptions: NextAuthOptions ={
+    providers: [
+        CredentialsProvider({
+           name:"Credentials",
+           credentials:{
+            email: {labe:"Email",type:"text"},
+            password: {label:"Password",type:"password"}
+           },
+           async authorize(credentials) {
+            if(!credentials?.email || !credentials?.password){
+              throw new Error("Email or password missing!")
+            }
+
+            try {
+              await connectToDatabase();
+              const user = await User.findOne({email:credentials.email})
+              if(!user){
+                throw new Error("User not found!")
+              }
+
+              const isValid = await bcrypt.compare(credentials.password, user.password)
+              if(!isValid){
+                throw new Error("Invalid Password!")
+              }
+
+              return{
+                id:user._id.toString(),
+                email:user.email
+              }
+            } catch (error) {
+              console.error("Error during authorization:", error);
+              throw error
+            }
+
+           }            
+        })
+    
+  
+    
+  ],
+}
